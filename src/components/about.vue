046c2ef9be1d3e22d04a023ee2f42468d958fc95
Wang Lei <1327578214@qq.com>
2020-12-19 23:04:20 +0800

    VueBlog

---
+++ src/components/about.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>王磊，北京交通大学软件学院1805班，本项目为web前端开发课程的第四次作业</div>
+    <div>联系方式：18703188717</div>
+    <div>QQ：    1327578214</div>
+    <div>Wechat：WL1327578214</div>
+  </div>
+</template>
